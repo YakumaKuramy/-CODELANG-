d101e4148a41723f77af5572a1c1717c29a1d720
Yakuma Kuramy <104542682+YakumaKuramy@users.noreply.github.com>
2023-05-30 16:54:12 -0300

    update
    Trilha digital (Ainda estou construindo as atividades propostas)

---
+++ javascript/primeiro.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!")
+console.log("Bem vindo ao meu primeiro codigo de Javascript!")
+// Primeiro comentario em linha 
+/*
+Segundo comentario, esse em  um bloco de linhas
+*/
